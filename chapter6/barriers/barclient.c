6c6376800b3a59c6ceaadc4938032777be210219
Peyman <pey.mortazavi@gmail.com>
2019-05-03 17:22:24 -0400

    added barrier server & client

--- chapter6/barriers/barclient.c
+++ chapter6/barriers/barclient.c
@@ -1,9 +1,69 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+
+#include "utils.h"
+
+
+int waitbarrier(char* name, char wb) {
+  char* request_name = get_request_barrier(name);
+  int request_fifo;
+  if ( (request_fifo = open(request_name, O_WRONLY)) == -1) {
+    fprintf(stderr, "Could not open the request fifo.\n");
+    free(request_name);
+    return -1;
+  }
+  if (write(request_fifo, &wb, 1) != 1) {
+    fprintf(stderr, "Could not write to the fifo.\n");
+    free(request_name);
+    close(request_fifo);
+    return -1;
+  }
+  free(request_name);
+  close(request_fifo);
+  // now wait for the response.
+  char* release_name = get_release_barrier(name);
+  int release_fifo;
+  if ( (release_fifo = open(release_name, O_RDONLY)) == -1) {
+    fprintf(stderr, "Could not open the release fifo file '%s' for reading.\n", release_name);
+    free(release_name);
+    return -1;
+  }
+  char c;
+  ssize_t len;
+  if ((len = read(release_fifo, &c, 1)) <= 0) {
+    if (len == 0) {
+      fprintf(stdout, "Received an eof!\n");
+      free(release_name);
+      close(release_fifo);
+      return 0;
+    }
+    if (len < 0) {
+      fprintf(stderr, "Encountered an error while reading from %s\n", release_name);
+      free(release_name);
+      close(release_fifo);
+      return -1;
+    }
+  }
+  fprintf(stdout, "BUFFER: %c\n", c);
+  free(release_name);
+  close(release_fifo);
+	return 0;
+}
 
 
 int main(int argc, char* argv[]) {
-	fprintf(stdout, "Client\n");
+  if (argc != 3) {
+    fprintf(stderr, "Usage : %s <name> <number>\n", argv[0]);
+    return 1;
+  }
+  char* name = argv[1];
+	fprintf(stdout, "waiting for barrier %s\n", name);
+  if (waitbarrier(argv[1], argv[2][0]) != 0) {
+    fprintf(stderr, "FATAL: failed wait\n");
+    return 1;
+  }
+  fprintf(stdout, "Released\n");
 	return 0;
 }
--- chapter6/barriers/barserver.c
+++ chapter6/barriers/barserver.c
@@ -1,9 +1,80 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <error.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+#include "utils.h"
+
+
+int read_until(int fd, void* buffer, int len) {
+	int counter = 0;
+	while (counter < len) {
+		ssize_t byte_count;
+		if ( (byte_count = read(fd, buffer + counter, len - counter)) == -1) {
+			fprintf(stderr, "bad error: %d %s\n", errno, strerror(errno));
+			return byte_count;
+		}
+		counter += byte_count;
+	}
+	return counter;
+}
 
 
 int main(int argc, char* argv[]) {
-	fprintf(stdout, "Server\n");
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s <barrier-name> <size>\n", argv[0]);
+		return 1;
+	}
+	char* name = argv[1];
+	int size = atoi(argv[2]);
+	if (size <= 0) {
+		fprintf(stderr, "Size has to be a valid number larger than one.\n");
+		return 1;
+	}
+	char* request_fifo;
+	char* release_fifo;
+	if ( (request_fifo = create_request_fifo(name)) == NULL ) {
+		fprintf(stderr, "Error while creating the request fifo.\n");
+		return 1;
+	}
+	fprintf(stdout, "Created %s\n", request_fifo);
+	if ( (release_fifo = create_release_fifo(name)) == NULL ) {
+		fprintf(stderr, "Error while creating the release fifo.\n");
+		return 1;
+	}
+	fprintf(stdout, "Created %s\n", release_fifo);
+	char buffer[size];
+	int request_fd = -1;
+	int release_fd = -1;
+	for (;;) {
+		fprintf(stdout, "Going to open the request fifo '%s' for reading.\n", request_fifo);
+		if ( (request_fd = open(request_fifo, O_RDONLY)) < 0) {
+			fprintf(stderr, "An error occured while opening the request fifo.\n");
+			return 1;
+		}
+		fprintf(stdout, "Going to read %d bytes.\n", size);
+		if (read_until(request_fd, &buffer, size) <= 0) {
+			close(request_fd);
+			fprintf(stderr, "An error occurred while reading from the request fifo.\n");
+			return 1;
+		}
+		close(request_fd);
+		fprintf(stdout, "Read %d bytes, releasing now.\n", size);
+		if ( (release_fd = open(release_fifo, O_WRONLY)) == -1) {
+			fprintf(stderr, "Could not open the release fifo %s for writing.\n", release_fifo);
+			return 1;
+		}
+		if (write(release_fd, &buffer, size) < 0) {
+			fprintf(stderr, "Could not write to the release fifo.\n");
+			close(release_fd);
+			return 1;
+		}
+		fprintf(stdout, "Released the barrier.\n");
+		close(release_fd);
+	}
 	return 0;
 }
