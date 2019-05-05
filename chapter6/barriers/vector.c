8c300e8038cc0f1bab54db21adf18239ba62bef9
Peyman <pey.mortazavi@gmail.com>
2019-05-05 00:08:18 -0400

    Added multibar server init code.

--- chapter6/barriers/vector.c
+++ chapter6/barriers/vector.c
@@ -1,5 +1,10 @@
-#include "vector.h"
 #include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#include "vector.h"
 
 
 void vec_init(vector_t* self) {
@@ -18,14 +23,13 @@ node_t* vec_find(vector_t* self, char* name) {
 }
 
 
-void vec_add(vector_t* self, char* name, size_t requirement) {
-  if (vec_find(self, name) != NULL) return;
+int vec_add(vector_t* self, char* name, pid_t proc) {
+  if (vec_find(self, name) != NULL) return -1;
 
   node_t* item = malloc(sizeof(node_t));
-  item->counter = 0;
   item->name = name;
   item->next = NULL;
-  item->requirement = requirement;
+  item->process_id = proc;
 
   if (self->head == NULL) {
     self->head = item;
@@ -35,6 +39,7 @@ void vec_add(vector_t* self, char* name, size_t requirement) {
     cursor->next = item;
   }
   self->len++;
+  return 0;
 }
 
 
@@ -48,4 +53,27 @@ void vec_del(vector_t* self, node_t* node) {
   free(node->name);
   free(node);
   self->len--;
+}
+
+
+void vec_dyn_iter(vector_t* self, int(*cb)(node_t* node)) {
+  node_t* cursor = self->head;
+  node_t* prior = NULL;
+  while(cursor != NULL) {
+    if (cb(cursor) != 0) {
+      // it means we have to delete this.
+      if (prior == NULL) {  // it means we're at HEAD.
+        self->head = cursor->next;
+      } else {
+        prior->next = cursor->next;  
+      }
+      self->len--;
+      free(cursor->name);
+      node_t* tmp = cursor;
+      cursor = cursor->next;
+      free(tmp);
+    } else {
+      cursor = cursor->next;
+    }
+  }
 }
--- chapter6/barriers/vector.h
+++ chapter6/barriers/vector.h
@@ -9,8 +9,7 @@
 typedef struct node_t {
   struct node_t* next;
   char* name;
-  size_t requirement;
-  size_t counter;
+  pid_t process_id;
 } node_t;
 
 
@@ -22,8 +21,10 @@ typedef struct vector_t {
 
 void vec_init(vector_t* self);
 node_t* vec_find(vector_t* self, char* name);
-void vec_add(vector_t* self, char* name, size_t requirement);
+int vec_add(vector_t* self, char* name, pid_t proc);
 void vec_del(vector_t* self, node_t* node);
+
+void vec_dyn_iter(vector_t* self, int(*cb)(node_t* node));
 
 
 #endif
