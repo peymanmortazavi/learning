6f22f0d8dc5ae3ec8335dc8018f8aaa1457c8db8
Peyman <pey.mortazavi@gmail.com>
2019-05-15 10:58:20 -0400

    client and server communicating through a request pipe

--- chapter6/client_server_fifo/fifo_client.c
+++ chapter6/client_server_fifo/fifo_client.c
@@ -1,8 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "utils.h"
 
 
 int main(int argc, char* argv[]) {
-    return 0;
+  // open the request fifo for writing.
+  int request_fifo;
+  while ((request_fifo = open("request", O_WRONLY)) <= 0 && errno == EINTR);
+  if (request_fifo <= 0) {
+    fprintf(stderr, "Coult not open the request fifo for reading: %s\n", strerror(errno));
+    return EXIT_FAILURE;
+  }
+  // now get the current pid.
+  pid_t mypid = getpid();
+  char* name = malloc(12 * sizeof(char));
+  int name_size = sprintf(name, "%d", mypid);
+  fprintf(stdout, "Creating response fifo '%s'\n", name);
+  if(create_response_fifo(name) != 0) {
+    fprintf(stderr, "Could not create response fifo '%s'\n", name);
+    return EXIT_FAILURE;
+  }
+  // now write this name to the request fifo so the server can learn about this.
+  ssize_t bytecount;
+  while((bytecount = write(request_fifo, name, name_size)) <= 0 && errno == EINTR);
+  if (bytecount <= 0) {
+    fprintf(stderr, "Error writing to the request fifo: '%s'\n", strerror(errno));
+    unlink(name);
+    return EXIT_FAILURE;
+  }
+  unlink(name);
+  return EXIT_SUCCESS;
 }
--- chapter6/client_server_fifo/fifo_server.c
+++ chapter6/client_server_fifo/fifo_server.c
@@ -15,25 +15,29 @@ int main(int argc, char* argv[]) {
     fprintf(stderr, "Could not create request fifo: %s\n", strerror(errno));
     return EXIT_FAILURE;
   }
-  // now open the fifo for reading
-  int request_fd;
-  while((request_fd = open("request", O_RDONLY)) != 0 && errno == EINTR);
-  if (request_fd <= 0) {
-    fprintf(stderr, "Error opening the request fifo: %s\n", strerror(errno));
-    return EXIT_FAILURE;
-  }
-  // okay now start an infinite loop working on this.
   char* buffer = malloc(256 * sizeof(char));
   ssize_t bytecount;
   for (;;) {
-    // now start reading from the file. Every time you read.
-    while((bytecount = read(request_fd, buffer, 255)) == -1 && errno == EINTR);
-    if (bytecount < 0) {
-      fprintf(stderr, "Could not read from the request fifo: %s\nexiting now\n.", strerror(errno));
-      break;
+    // now open the fifo for reading
+    int request_fd;
+    while((request_fd = open("request", O_RDONLY)) != 0 && errno == EINTR);
+    if (request_fd <= 0) {
+      fprintf(stderr, "Error opening the request fifo: %s\n", strerror(errno));
+      return EXIT_FAILURE;
     }
-    buffer[bytecount] = '\n';
-    write(STDOUT_FILENO, buffer, bytecount + 1);
+    // okay now start an infinite loop working on this.
+    for (;;) {
+      // now start reading from the file. Every time you read.
+      while((bytecount = read(request_fd, buffer, 255)) < 0 && errno == EINTR);
+      if (bytecount < 0) {
+        fprintf(stderr, "Could not read from the request fifo: %s\nexiting now\n.", strerror(errno));
+        break;
+      } else if (bytecount == 0) {
+        fprintf(stdout, "No clients are listening to the fifo, stop read operations.\n");
+        break;
+      }
+      fprintf(stdout, "Received response pipe '%s', going to read from it.\n", buffer);
+    }
   }
   return 0;
 }
