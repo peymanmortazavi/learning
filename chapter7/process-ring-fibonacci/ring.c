6cc058d8f7b7f3fa2fc2b7c1a449960478c7bfaf
Peyman <pey.mortazavi@gmail.com>
2019-05-17 20:55:50 -0400

    completed fib assignment

--- chapter7/process-ring-fibonacci/ring.c
+++ chapter7/process-ring-fibonacci/ring.c
@@ -4,13 +4,45 @@
 #include <string.h>
 
 
-void process(int id) {
-    for(int i = 0; i < 10; i++) {
-        int result;
-        write(STDOUT_FILENO, &i, sizeof(i));
-        ssize_t rc = read(STDIN_FILENO, &result, sizeof(result));
-        fprintf(stderr, "OUTPUT @ %d: %d\n", id, result);
+int* calc_next(int id) {
+    char* buffer = malloc(256 * sizeof(buffer));
+    ssize_t rbytecount = read(STDIN_FILENO, buffer, 256);
+    buffer[rbytecount] = 0;
+    fprintf(stderr, "%d: got %s\n", id, buffer);
+    int* result = malloc(2 * sizeof(int));
+    for(unsigned int index = 0; index < rbytecount; index++) {
+        if (buffer[index] == ' ') {
+            buffer[index] = 0;
+            result[0] = atoi(buffer + index + 1);
+            result[1] = atoi(buffer) + result[0];
+            buffer[index] = ' ';
+            break;
+        }
     }
+    free(buffer);
+    return result;
+}
+
+
+void process_client(int id) {
+    char* buffer = malloc(256 * sizeof(char));
+    int* numbers = calc_next(id);
+    int count = sprintf(buffer, "%d %d", numbers[0], numbers[1]);
+    buffer[count] = 0;
+    ssize_t wbytecount = write(STDOUT_FILENO, buffer, count);
+    fprintf(stderr, "%d: wrote %s which is %ld bytes to the next item.\n", id, buffer, wbytecount);
+    free(buffer);
+    free(numbers);
+}
+
+
+void process_server(int id) {
+    static char* starting_point = "1 1";
+    ssize_t wbytecount = write(STDOUT_FILENO, starting_point, 3);
+    fprintf(stderr, "server wrote %ld bytes.\n", wbytecount);
+    int* numbers = calc_next(id);
+    fprintf(stderr, "RESULT: %d\n", numbers[1]);
+    free(numbers);
 }
 
 
@@ -39,6 +71,10 @@ int main(int argc, char* argv[]) {
             break;
         }
     }
-    process(id);
+    if (id == 0) {
+        process_server(id);
+    } else {
+        process_client(id);
+    }
     return EXIT_SUCCESS;
 }
