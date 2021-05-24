8b42ba76186eb7fbae4786f5a72757dcab4c6b58
Peyman Mortazavi <peyman.mortazavi@mariadb.com>
2021-05-24 15:08:47 -0400

    adding exercise 5.10

---
+++ go/GoBook/5.10/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+/*
+	Rewrite topoSort to use maps instead of slices and eliminate the initial sort.
+	Verify that the results, though nondeterministic, are valid topological orderings.
+*/
+
+// prereqs maps computer science courses to their prerequisites.
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func main() {
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
+}
+
+func topoSort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items []string)
+
+	visitAll = func(items []string) {
+		for _, k := range items {
+			if !seen[k] {
+				seen[k] = true
+				visitAll(m[k])
+				order = append(order, k)
+			}
+		}
+	}
+
+	keys := []string{}
+	for key := range m {
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+	visitAll(keys)
+	return order
+}
